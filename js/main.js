3d3eaf2661c9847b7e1385f9f4e38448efa3c956
Gerardo Sepúlveda <gerardoj.sepulveda@gmail.com>
2021-03-24 19:18:10 -0600

    Nuevas páginas y ajustes generales a estilos

--- js/main.js
+++ js/main.js
@@ -8,4 +8,33 @@ $(document).ready(function(){
     $(this).addClass('plan_selected');
     $(this).siblings().removeClass('plan_selected');
   })
+  //CERRAR POPUP
+  $('.close_popup').click(function(){
+    $('body').removeClass('popupOpen')
+  })
+  $('#show_payment_modal').click(function(event){
+    event.preventDefault();
+    $('body').addClass('popupOpen')
+  })
+  //HABILITAR COMPROBACIÓN DE DOMINIO AL SELECCIONAR DOMINIO
+  $('#comprar_dominio').click(function(){
+    if (!$("#comprar_dominio").is(':checked')) {
+       $('.contratar_dominio').addClass('input_disabled');
+       $('.legales_dominio').hide();
+    }
+    else {
+       $('.contratar_dominio').removeClass('input_disabled');
+       $('.legales_dominio').css('display','grid');
+    }
+  });
+  $('#dominio_propio').click(function(){
+    if (!$("#comprar_dominio").is(':checked')) {
+       $('.contratar_dominio').addClass('input_disabled');
+       $('.legales_dominio').hide();
+    }
+    else {
+       $('.contratar_dominio').removeClass('input_disabled');
+       $('.legales_dominio').css('display','grid');
+    }
+  });
 })
